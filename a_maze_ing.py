8d2bace01c0bad2415bdb48aa986cbd9956b1258
raqroca- <raqroca-@student.42madrid.com>
2026-06-27 17:03:15 +0200

    fix: try except

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -10,47 +10,50 @@ from display import MazeDisplay
 
 
 def main() -> None:
-    config = config_parser.parse_config("config.txt")
+    try:
+        config = config_parser.parse_config("config.txt")
 
-    width = cast(int, config["width"])
-    height = cast(int, config["height"])
-    entry = cast(tuple[int, int], config["entry"])
-    exit_cell = cast(tuple[int, int], config["exit"])
-    perfect = cast(bool, config["perfect"])
-    seed = cast(int | None, config.get("seed"))
-    output_file = cast(str, config["output_file"])
+        width = cast(int, config["width"])
+        height = cast(int, config["height"])
+        entry = cast(tuple[int, int], config["entry"])
+        exit_cell = cast(tuple[int, int], config["exit"])
+        perfect = cast(bool, config["perfect"])
+        seed = cast(int | None, config.get("seed"))
+        output_file = cast(str, config["output_file"])
 
-    generator = MazeGenerator(
-        width=width,
-        height=height,
-        entry=entry,
-        exit=exit_cell,
-        perfect=perfect,
-        seed=seed,
-    )
+        generator = MazeGenerator(
+            width=width,
+            height=height,
+            entry=entry,
+            exit=exit_cell,
+            perfect=perfect,
+            seed=seed,
+        )
 
-    map = generator.machete()
-    path_coordinates = find_short_path(map, entry, exit_cell)
+        map = generator.machete()
+        path_coordinates = find_short_path(map, entry, exit_cell)
 
-    export_maze_to_file(
-        filename=output_file,
-        entry=entry,
-        exit_cell=exit_cell,
-        maze_map=map,
-        path=path_coordinates
-    )
-    print(f"[INFO] File '{output_file}' exported successfully.")
+        export_maze_to_file(
+            filename=output_file,
+            entry=entry,
+            exit_cell=exit_cell,
+            maze_map=map,
+            path=path_coordinates
+        )
+        print(f"[INFO] File '{output_file}' exported successfully.")
 
-    print("[INFO] Opening MiniLibX graphical display...")
-    display = MazeDisplay(  # noqa: F841
-        width=width,
-        height=height,
-        entry=entry,
-        exit=exit_cell,
-        grid=map,
-        generator=generator,
-        filename=output_file
-    )
+        print("[INFO] Opening MiniLibX graphical display...")
+        display = MazeDisplay(  # noqa: F841
+            width=width,
+            height=height,
+            entry=entry,
+            exit=exit_cell,
+            grid=map,
+            generator=generator,
+            filename=output_file
+        )
+    except ValueError as e:
+        print(e)
 
 
 if __name__ == "__main__":
